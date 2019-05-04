7ab09fc67e79016125cfbbc63aa842d7c164d80f
Yifei He <yifei@hesyifei.com>
2019-05-04 14:36:21 -0700

    add basic calendar in CalendarView

--- src/pages/CalendarView.js
+++ src/pages/CalendarView.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
 import NotFound from './NotFound';
 import { fetchPapersByYear } from '../helpers/papers';
+
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = BigCalendar.momentLocalizer(moment);
 
 class CalendarView extends React.Component {
   constructor(props) {
@@ -30,8 +36,37 @@ class CalendarView extends React.Component {
     return (
       <div className="CalendarView">
         CalendarView{this.props.match.params.year}, {this.props.match.params.month}
+        <div>
+          <BigCalendar
+            localizer={localizer}
+            events={[
+              {
+                start: new Date(),
+                end: new Date(),
+                title: "Some title"
+              },
+              {
+                start: new Date(moment().add(1, "days")),
+                end: new Date(moment().add(1, "days")),
+                title: "Some title"
+              }
+            ]}
+            startAccessor="start"
+            endAccessor="end"
+            onSelectEvent={(event, e) => this.paperOnSelect(event, e)}
+            views={[ "month" ]}
+            style={{ "height": 500 }}
+          />
+        </div>
       </div>
     );
+  }
+
+  paperOnSelect(event, e) {
+    let selectedDate = event.start;
+    let selectedDateString = moment(selectedDate).format('YYYY-MM-DD');
+    console.log(selectedDateString);
+    this.props.history.push('/paper/' + selectedDateString);
   }
 }
 
