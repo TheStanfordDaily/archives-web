cf83513199d69682e389cc4496013adfdd64b610
Yifei He <yifei@hesyifei.com>
2019-05-03 21:52:46 -0700

    support fetchAllPapers from metadata.json

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import OpenSeadragon from 'openseadragon';
 import Paper from './classes/Paper';
+import { STRINGS } from './helpers/constants';
 
 class App extends React.Component {
   async componentDidMount() {
@@ -14,7 +15,10 @@ class App extends React.Component {
     );
     console.log(openseadragonImagesFolderPath);*/
 
-    let testPaper = new Paper(1920, 10, 1, "data.2012-aug/data/stanford/1920/10/01_01/", "Stanford_Daily_19201001_0001-METS.xml");
+    await this.fetchAllPapers();
+
+    //let testPaper = new Paper(1920, 10, 1, "data.2012-aug/data/stanford/1920/10/01_01/", "Stanford_Daily_19201001_0001-METS.xml");
+    let testPaper = this.allPapers[10000];
     let results = await testPaper.getPages();
     console.log(results);
 
@@ -53,6 +57,26 @@ class App extends React.Component {
         <div id="openseadragon1" style={{ "width": 800, "height": 600}} />
       </div>
     );
+  }
+
+  async fetchAllPapers() {
+    this.allPapers = [];
+    let results = await fetch(STRINGS.FILE_SERVER_URL + "metadata.json").then(e => e.json());
+    //console.log(results);
+    for (var year in results) {
+      for (var month in results[year]) {
+        for (var day in results[year][month]) {
+          let fullFilePath = results[year][month][day];
+          // https://stackoverflow.com/a/5555607/2603230
+          var folderPath = fullFilePath.substring(0, fullFilePath.lastIndexOf("/") + 1);
+          var metsFilePath = fullFilePath.substring(fullFilePath.lastIndexOf("/") + 1, fullFilePath.length);
+          let paper = new Paper(Number(year), Number(month), Number(day), folderPath, metsFilePath);
+          //console.log(fullFilePath);
+          this.allPapers.push(paper);
+        }
+      }
+    }
+    console.log(this.allPapers);
   }
 }
 
