e7876f19107dfb9dfadfab78a41c7be603487992
Yifei He <yifei@hesyifei.com>
2019-05-03 23:38:03 -0700

    check if year exists in CalendarView

--- src/pages/CalendarView.js
+++ src/pages/CalendarView.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import NotFound from './NotFound';
+import { fetchPapersByYear } from '../helpers/papers';
 
 class CalendarView extends React.Component {
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = { calendarNotFound: false };
+  }
+
+  async componentDidMount() {
+    if (!this.props.match.params.month) {
+      let yearData = await fetchPapersByYear(this.props.match.params.year);
+      console.log(yearData);
+      if (yearData === null) {
+        this.setState({ calendarNotFound: true });
+      }
+    }
   }
 
   componentWillUnmount() {
   }
 
   render() {
+    if (this.state.calendarNotFound) {
+      return (
+        <NotFound />
+      );
+    }
     return (
-      <div className="NotFound">
+      <div className="CalendarView">
         CalendarView{this.props.match.params.year}, {this.props.match.params.month}
       </div>
     );
