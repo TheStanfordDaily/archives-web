64151589cf8cbec0233f7084c6956d80bf63db72
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-05-15 17:29:29 -0700

    feat: handle simple date range

--- src/helpers/search.js
+++ src/helpers/search.js
@@ -22,6 +22,13 @@ function createPath({century, decade, year, month, day, pathSuffix}) {
     return `path:${pathPrefix}${pathSuffix}`;
 }
 
+/*
+ * Rounds *i* to the nearest *n*.
+ */
+function roundToNearest(i, n) {
+    return Math.round(i / n) * n;
+}
+
 export function createSearchQuery({year_start, year_end, year, month, day, type, query}) {
     let pathSuffix = "*.txt";
     if (type) {
@@ -32,7 +39,24 @@ export function createSearchQuery({year_start, year_end, year, month, day, type,
         path = createPath({century: String(year).substr(0, 2), decade: String(year).substr(0, 3), year, month, day, pathSuffix});
     }
     else if (year_start && year_end) {
-        path = createPath({year, month, day, pathSuffix});
+        let paths = [];
+        let nearest_start = roundToNearest(year_start, 100);
+        let nearest_end = roundToNearest(year_end, 100);
+        if (nearest_end - nearest_start === 100) {
+            paths.push(
+                createPath({century: String(nearest_start).substr(0, 2), pathSuffix})
+            );
+        }
+        else {
+            let nearest_start = roundToNearest(year_start, 10);
+            let nearest_end = roundToNearest(year_end, 10);
+            if (nearest_end - nearest_start === 10) {
+                paths.push(
+                    createPath({century: String(nearest_start).substr(0, 2), decade: String(nearest_start).substr(0, 3), pathSuffix})
+                );
+            }
+        }
+        path = paths.join(" ");
     }
     else {
         path = createPath({year, month, day, pathSuffix});
