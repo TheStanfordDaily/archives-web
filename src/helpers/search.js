32d96a9afdcbcf55714fac53c6e32af431637a2b
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-05-15 17:20:06 -0700

    refactor path code more

--- src/helpers/search.js
+++ src/helpers/search.js
@@ -1,8 +1,14 @@
 
 function createPath({century, decade, year, month, day, pathSuffix}) {
     let pathPrefix = "";
+    if (century) {
+        pathPrefix += `/${century}xx`;
+    }
+    if (decade) {
+        pathPrefix += `/${decade}x`;
+    }
     if (year) {
-        pathPrefix += `/${century}xx/${decade}x/${year}y`;
+        pathPrefix += `/${year}y`;
     }
     if (month) {
         pathPrefix += `/${month}m`;
@@ -21,6 +27,15 @@ export function createSearchQuery({year_start, year_end, year, month, day, type,
     if (type) {
         pathSuffix = `*.${type}.txt`;
     }
-    let path = createPath({century: String(year).substr(0, 2), decade: String(year).substr(0, 3), year, month, day, pathSuffix});
+    let path;
+    if (year) {
+        path = createPath({century: String(year).substr(0, 2), decade: String(year).substr(0, 3), year, month, day, pathSuffix});
+    }
+    else if (year_start && year_end) {
+        path = createPath({year, month, day, pathSuffix});
+    }
+    else {
+        path = createPath({year, month, day, pathSuffix});
+    }
     return `${path} ${query}`;
 }
