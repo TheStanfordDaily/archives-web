44578832b34e5f3bd10d9e5208cb327c755324df
Yifei He <yifei@hesyifei.com>
2019-05-19 14:02:46 -0700

    add AllYearView

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 import Home from './pages/Home'
 import TSDNavbar from './pages/components/TSDNavbar'
 import SearchView from './pages/SearchView'
+import AllYearView from './pages/AllYearView'
 import CalendarView from './pages/CalendarView'
 import PaperView from './pages/PaperView'
 import NotFound from './pages/NotFound'
@@ -27,6 +28,7 @@ class App extends React.Component {
           <div className="MainView">
             <Switch>
               <Route path={STRINGS.ROUTE_ROOT} exact component={Home} />
+              <Route path={STRINGS.ROUTE_CALENDAR_PREFIX} strict exact component={AllYearView} />
               <Route path={STRINGS.ROUTE_CALENDAR_PREFIX + ":year(\\d{4})/:month(\\d{2})/"} strict exact component={CalendarView} />
               <Route path={STRINGS.ROUTE_PAPER_PREFIX + ":year(\\d{4})-:month(\\d{2})-:day(\\d{2})"} strict exact component={PaperView} />
               <Route path={STRINGS.ROUTE_SEARCH_PREFIX} strict exact component={SearchView}/>
---
+++ src/pages/AllYearView.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import moment from 'moment'
+import Loading from './components/Loading';
+import { fetchMetadata, isMonthInMetaData } from '../helpers/papers';
+import { STRINGS } from '../helpers/constants';
+
+class AllYearView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true, allPapers: [] };
+  }
+
+  async componentDidMount() {
+    let allPapers = await fetchMetadata();
+    this.setState({ allPapers: allPapers, loading: false });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <Loading />
+      );
+    }
+
+    console.log(this.state.allPapers);
+
+    const allYears = Object.keys(this.state.allPapers);
+
+    console.log(allYears[0]);
+    console.log(allYears[allYears.length - 1]);
+
+    // https://stackoverflow.com/q/22876978/2603230
+    let yearElements = [];
+    for (let year = allYears[0]; year <= allYears[allYears.length - 1]; year++) {
+      let monthsElements = [];
+      for (let month = 0; month <= 11; month++) {
+        let dateMoment = moment({ year: year, month: month });
+        monthsElements.push(
+          <div key={month} className="EachMonth">
+            <span className={isMonthInMetaData(this.state.allPapers, dateMoment) ? "EachMonthLink" : "EachMonthNoLink"} onClick={() => { this.goToMonth(dateMoment); }}>
+              {moment.monthsShort(month)}
+            </span>
+          </div>
+        );
+      }
+      yearElements.push(
+        <div key={year} className="EachYear">
+          <h2 className="YearName">{year}</h2>
+          <div className="YearMonths">{monthsElements}</div>
+        </div>
+      );
+    }
+
+    return (
+      <div className="AllYearMainView">
+        <div className="AllYears">
+          {yearElements}
+        </div>
+      </div>
+    );
+  }
+
+  goToMonth(dateMoment) {
+    if (isMonthInMetaData(this.state.allPapers, dateMoment)) {
+      let yearString = dateMoment.format('YYYY');
+      let monthString = dateMoment.format('MM');
+      this.props.history.push(STRINGS.ROUTE_CALENDAR_PREFIX + yearString + "/" + monthString + "/");
+    }
+  }
+}
+
+export default AllYearView;
