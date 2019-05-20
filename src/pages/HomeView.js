3e6d5c961999e1b371729aaba8507295def8aec2
Yifei He <yifei@hesyifei.com>
2019-05-19 22:27:19 -0700

    use constant in HomeView

--- src/pages/HomeView.js
+++ src/pages/HomeView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { IoIosSearch } from "react-icons/io";
+import { STRINGS } from '../helpers/constants'
 
 class HomeView extends React.Component {
   componentDidMount() {
@@ -11,21 +12,18 @@ class HomeView extends React.Component {
   render() {
     return (
       <div className="HomeMainView container mt-4 text-left">
-
         <div className="row">
           <div className="col-12 col-sm-4">
             <h2>About this collection</h2>
             <p>Welcome to The Stanford Daily archive! We are proud to present the collection of issues dating back to 1892. Since its founding, the Daily has informed, educated and entertained Stanford students and the surrounding community.</p>
-
             <p>Providing a firsthand account of life at Stanford from 1892-today, the archive is an invaluable resource for anyone looking to explore American history as seen through the eyes of undergraduates. Issues are presented as originally printed, accompanied by a sidebar featuring articles in plain text. The collection is entirely searchable, and registration is only necessary to make corrections in the plain text.</p>
-
             <p>This collection contains 18,931 issues comprising 143,685 pages and 1,013,592 articles.</p>
           </div>
           <div className="col-12 col-sm-4">
             <h2>Search the collection</h2>
-            <form action="/search" className="mt-4">
+            <form action={STRINGS.ROUTE_SEARCH_PREFIX} className="mt-4">
               <div className="input-group">
-                <input type="text" class="form-control" name="q" required />
+                <input type="text" className="form-control" name="q" placeholder="Search&hellip;" required />
                 <div className="input-group-append">
                   <button type="submit" className="form-control HomeSearchButton"><IoIosSearch /></button>
                 </div>
