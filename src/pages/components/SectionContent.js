d6fb945b8606107547fd8ad1f9149aeda0b275f6
Yifei He <yifei@hesyifei.com>
2019-05-24 15:38:16 -0700

    add support for displaying article title, subtitle, and author

--- src/pages/components/SectionContent.js
+++ src/pages/components/SectionContent.js
@@ -5,7 +5,7 @@ import { STRINGS } from '../../helpers/constants'
 class SectionContent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { loading: true, paperContent: null };
+    this.state = { loading: true, sectionContent: null };
   }
 
   componentDidMount() {
@@ -22,11 +22,11 @@ class SectionContent extends React.Component {
   fetchSectionContent() {
     if (!this.props.date) {
       console.warn("No this.props.date!");
-      this.setState({ loading: false, paperContent: null });
+      this.setState({ loading: false, sectionContent: null });
       return;
     }
     if (!this.props.section) {
-      this.setState({ loading: false, paperContent: null });
+      this.setState({ loading: false, sectionContent: null });
       return;
     }
 
@@ -34,7 +34,14 @@ class SectionContent extends React.Component {
     console.log(serverURL);
     fetch(serverURL).then(e => e.text()).then(e => {
       console.log(e);
-      this.setState({ loading: false, paperContent: e });
+      let allLines = e.split("\n");
+
+      let sectionContent = {};
+      sectionContent.title = allLines[0].substr(2); // Remove "# ".
+      sectionContent.subtitle = allLines[1].substr(3); // Remove "## ".
+      sectionContent.author = allLines[2].substr(4); // Remove "### ".
+      sectionContent.content = allLines.slice(3).join("\n"); // First three lines (title, subtitle, and author) are not included in content
+      this.setState({ loading: false, sectionContent: sectionContent });
     });
   }
 
@@ -45,16 +52,22 @@ class SectionContent extends React.Component {
       );
     }
 
-    if (!this.state.paperContent) {
+    if (!this.state.sectionContent) {
       return (
         <p><i>Select an article in the document viewer.</i></p>
       );
     }
 
     return (
-      <pre>
-        {this.state.paperContent}
-      </pre>
+      <div className="SectionContent">
+        {this.state.sectionContent.title && <h3>{this.state.sectionContent.title}</h3>}
+        {this.state.sectionContent.subtitle && <h5>{this.state.sectionContent.subtitle}</h5>}
+        {this.state.sectionContent.author && <p className="Author">By <strong>{this.state.sectionContent.author}</strong></p>}
+        <hr />
+        <pre>{/* TODO: increase `pre` readability: see `/paper/1973-01-30#page=1&section%5B%5D=MODSMD_ARTICLE4` */}
+          {this.state.sectionContent.content}
+        </pre>
+      </div>
     );
   }
 }
